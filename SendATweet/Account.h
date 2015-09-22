af11bdf7604fd1b6add8277d04941008f9017d73
nice295 <nice295@gmail.com>
2015-09-22 21:19:22 +0900

    delete content

--- SendATweet/Account.h
+++
@@ -1,8 +0,0 @@
-const String TWITTER_ACCESS_TOKEN = "3443050645-v04RFKFKT1uGgGP7YPyWub15OrZAUaSCdbUGotg";
-const String TWITTER_ACCESS_TOKEN_SECRET = "iQphECnTIBzfvIMbiK4awy6TZDsxomluhlqnIwtbMqUPu";
-const String TWITTER_API_KEY = "yIzvAt1PD5uBLUY3UYFscAArN";
-const String TWITTER_API_SECRET = "Ib6VA4CycHh9uJNT1uUtpcFsEt6oeQkkOGbxgFS1FoSO62S4S9";
-
-#define TEMBOO_ACCOUNT "nice295"  // your Temboo account name 
-#define TEMBOO_APP_KEY_NAME "myFirstApp"  // your Temboo app key name
-#define TEMBOO_APP_KEY  "0aa8c4ce44b54d4f838e84aa254af9f3"  // your Temboo app key
